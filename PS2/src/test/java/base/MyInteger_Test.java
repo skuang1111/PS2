5a0d0f56563decc1bcb9832d88ee96c641140397
KuangS <skuang@udel.edu>
2016-02-26 20:46:54 -0500

    MyInteger Added

--- PS2/src/test/java/base/MyInteger_Test.java
+++ PS2/src/test/java/base/MyInteger_Test.java
@@ -14,6 +14,9 @@ public class MyInteger_Test {
 	public static void setUpBeforeClass() throws Exception {
 	}
 
+	static MyInteger myInt1 = new MyInteger(5);
+	static MyInteger myInt2 = new MyInteger(10);
+	
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
@@ -25,10 +28,25 @@ public class MyInteger_Test {
 	@After
 	public void tearDown() throws Exception {
 	}
+	
 
 	@Test
 	public void test() {
-		assertTrue(1==1);
+		assertFalse(myInt1.isEven());
+		assertTrue(myInt1.isOdd());
+		assertTrue(myInt1.isPrime());
+		assertTrue(MyInteger.isEven(10));
+		assertFalse(MyInteger.isOdd(10));
+		assertFalse(MyInteger.isPrime(10));
+		assertFalse(MyInteger.isEven(myInt1));
+		assertTrue(MyInteger.isOdd(myInt1));
+		assertTrue(MyInteger.isPrime(myInt1));
+		assertTrue(myInt1.equals(5));
+		assertFalse(myInt1.equals(myInt2));
 	}
+	
+	
+
+	
 
 }
